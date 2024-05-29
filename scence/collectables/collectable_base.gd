d9658f345e281cc4c4c011a49086224071a5c76b
praveenpkg8 <praveepkg8@gmail.com>
2024-05-30 01:39:05 +0530

    Added: huge refactoring

--- scence/collectables/collectable_base.gd
+++ scence/collectables/collectable_base.gd
@@ -1,11 +1,14 @@
+class_name CollectableBase
 extends Area2D
 
 signal on_player_interact
 
-@onready var sprite:AnimatedSprite2D = $AnimatedSprite2D
-
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @export var collectable_value: float = 1.00
+
+
+
 
 func _on_body_entered(body: CharacterBody2D):
 	if body.is_in_group("Player"):
-		emit_signal("on_player_interact")
+		emit_signal("on_player_interact", collectable_value)
--- scence/collectables/virus_essence/virus_essence.gd
+++ scence/collectables/virus_essence/virus_essence.gd
@@ -1,10 +1,13 @@
-extends "res://scence/collectables/collectable_base.gd"
+extends CollectableBase
 
+var grade_level: float = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	sprite.play('default')
+	grade_level = randf_range(0, 3)
+	
 
-func _on_on_player_interact():
-	GlobalSignals.emit_signal('picked_up_collectable', collectable_value)
+func _on_on_player_interact(col_value):
+	GlobalSignals.emit_signal('picked_up_collectable', col_value * grade_level)
 	queue_free()
